41d09cd34ddabe527d06c6daf6f7898cebf7a954
Scott Wilson <scottwilson1020@gmail.com>
2020-09-01 18:30:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,8 +41,13 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  
-  def new_from_db
-    
-  end 
+
+   def self.new_from_db(row)
+    # create a new Student object given a row from the database	   
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end	
 end
