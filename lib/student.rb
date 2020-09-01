f0dfb31cef57537e6d7d978b03e1d24b4d66daa1
Scott Wilson <scottwilson1020@gmail.com>
2020-09-01 19:04:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,6 +87,14 @@ def self.find_by_name(name)
     DB[:conn].execute(sql)
   end 
   
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT COUNT (grade=12) FROM students 
+    SQL 
+    
+  DB[:conn].execute(sql)
+    
+  end
   
   
 end
